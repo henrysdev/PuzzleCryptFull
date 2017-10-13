cbcf3e813ea55d15c07c3cb0e09ee5a0d971fc4a
crackerjacks <henrysdev@gmail.com>
2017-10-13 01:15:07 -0500

    Linked IOManager with Debugger

--- src/DebugLogger.java
+++ src/DebugLogger.java
@@ -3,13 +3,19 @@ import java.util.Stack;
 public class DebugLogger {
     private static Stack<String> logs = new Stack<String>();
 
-    public static void log(String entry)
-    {
+    public static void log(String entry) {
         logs.push(entry);
     }
 
-    public static Stack<String> getLogs()
-    {
+    public static Stack<String> getLogs() {
         return logs;
+    }
+
+    public static String printLogs() {
+        String formattedLogs = "";
+        while(!logs.empty()) {
+            formattedLogs = formattedLogs.concat(logs.pop() + '\n');
+        }
+        return formattedLogs;
     }
 }
