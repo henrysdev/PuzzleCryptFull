284e3ec44cbd50e88559f4fe5b37644063be8b3e
Tyler Springer <tylersp@amazon.com>
2017-11-06 20:56:14 -0800

    Refactored Shard.java to use new byte[] collector.

--- src/main/java/Shard.java
+++ src/main/java/Shard.java
@@ -5,13 +5,12 @@ import java.util.stream.Stream;
 @Data
 public class Shard {
 
-    private final Byte[] payload;
-    private final Byte[] IV;
-    private final Byte[] HMAC;
+    private final byte[] payload;
+    private final byte[] IV;
+    private final byte[] HMAC;
 
     public byte[] toFragment(){
         return Stream.of(IV, payload, HMAC)
-            .collect(Utils.byteCollector())
-            .toByteArray();
+            .collect(Utils.byteCollector());
     }
 }
